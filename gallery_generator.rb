33d8b915f0779d038b0d0708a379f64a2bce0f76
Kyle Marsh <kyle.l.marsh+github@gmail.com>
2014-09-14 20:49:33 -0700

    Create individual image pages
    For every file in an album directory, create an HTML page to view the
    image and link to it from the album page.
    
    Fixes #4

--- gallery_generator.rb
+++ gallery_generator.rb
@@ -1,8 +1,23 @@
 module Jekyll
+	class ImagePage < Page
+		# An image page
+		def initialize(site, base, dir, name, image)
+			@site = site
+			@base = base
+			@dir = dir
+			@name = name # Name of the generated page
+
+			self.process(@name)
+			self.read_yaml(File.join(base, '_layouts'), 'image_page.html')
+			self.data['title'] = "#{image}"
+			self.data['img_src'] = image
+		end
+	end
+
 	class AlbumPage < Page
 		# An album page
 		#	Just a UL of images for now
-		def initialize(site, base, dir, parent, album)
+		def initialize(site, base, dir, parent)
 			@site = site
 			@base = base
 			@dir = dir
@@ -10,14 +25,24 @@ module Jekyll
 
 			self.process(@name)
 			self.read_yaml(File.join(base, '_layouts'), 'album_index.html')
-			self.data['title'] = "#{album}"
+			self.data['title'] = "#{dir}"
 
 			self.data['images'] = []
-			d = Dir.open(File.join(parent, album))
+			d = Dir.open(File.join(parent, dir))
 			d.each do |file|
 				next if /^\.\.?/ =~ file
-				#TODO: Do we want to generate thumbnails to put on this page?
-				self.data['images'] << File.join(d.path, file)
+				#TODO: Skip non-image files
+				ext = File.extname(file)
+				rel_link = "#{File.basename(file, ext)}_#{File.extname(file)[1..-1]}.html"
+				img_src = "#{File.join(d.path, file)}"
+				image_data = {
+					'src' => img_src,
+					'rel_link' => rel_link
+				}
+				self.data['images'] << image_data
+
+				# Create image page
+				site.pages << ImagePage.new(site, base, dir, rel_link, img_src)
 			end
 		end
 	end
@@ -32,7 +57,7 @@ module Jekyll
 				d.each do |album|
 					next if /^\.\.?$/ =~ album
 					# TODO: split to recursive helper to handle sub-albums
-					site.pages << AlbumPage.new(site, site.source, album, d.to_path, album)
+					site.pages << AlbumPage.new(site, site.source, album, d.to_path)
 				end
 			end
 		end
